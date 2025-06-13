0e9f680dba47214b0af7cb172ddd7e038f910fab
arafatrahat94 <arafatrahat94@gmail.com>
2025-06-13 11:53:07 +0600

    added icons

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,9 @@
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [first, setfirst] = useState(false);
   return (
     <View
       style={{
@@ -14,6 +17,14 @@ export default function Index() {
       <Text className="text-3xl font-spaceGrotesk">Photography</Text>
       <Text className="text-5xl font-banglaHeading">ছবিগ্রাফি</Text>
       <Text className="text-3xl font-banglaSubHeading">ফটোগ্রাফি </Text>
+      <Feather
+        onPress={() => {
+          setfirst(!first);
+        }}
+        name="home"
+        size={34}
+        color={first ? "red" : "green"}
+      />
     </View>
   );
 }
